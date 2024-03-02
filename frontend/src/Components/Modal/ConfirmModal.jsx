d230bfebfb78f1c354352b6a5478e877b4dbaed7
Monika504 <karkimonika56@gmail.com>
2024-03-03 00:20:16 +0545

    post data

--- frontend/src/Components/Modal/ConfirmModal.jsx
+++ frontend/src/Components/Modal/ConfirmModal.jsx
@@ -12,35 +12,48 @@ const ConfirmModal = ({ showModal, closeModal, dataSummary, Question }) => {
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-  
-   
-    const postData = { dataSummary };
-  
-    try {
-      
-      const response = await axios.post('http://127.0.0.1:8000/bookapi/', postData);
-  
-      // Log the response 
-      console.log('Server response:', response.data);
-  
-      // Close the modal after submitting
-      closeModal();
-    } catch (error) {
-      // Handle error appropriately
-      console.error('Error submitting data:', error);
-    }
+  event.preventDefault();
+
+  const postData = {
+    b_uid: "", // You might generate this value on the server-side or using some other mechanism
+    book_name: dataSummary.title,
+    author: dataSummary.author,
+    volume: dataSummary.category,
   };
+
+  try {
+    const response = await axios.post(
+      "http://127.0.0.1:8000/bookapi/",
+      postData, // No need to stringify, axios handles it for you
+      {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    );
+    console.log("Server data:", postData);
+    console.log("Server response:", response.data);
+
+    closeModal();
+  } catch (error) {
+    console.error("Error submitting data:", error);
+  }
+};
+
 
   return (
     <div className={`modal-overlay ${showModal ? 'modal-open' : ''}`} onClick={handleDialogClick}>
       <dialog open={showModal} className="modal">
         <div id="scancard" onClick={handleContentClick}>
           <h3 id="text2">{Question}</h3>
-          <h3 style={{fontWeight:"normal", margin:"40px"}}>{dataSummary}</h3>
+          <h3 style={{fontWeight:"normal", margin:"40px"}}>
+            Title: {dataSummary.title}<br />
+            Author: {dataSummary.author}<br />
+            Category: {dataSummary.category}
+          </h3>
           <div>
-          <button className="btn" onClick={handleSubmit}>Submit</button>
-          <button className="btn" onClick={closeModal}>Close</button>
+            <button className="btn" onClick={handleSubmit}>Submit</button>
+            <button className="btn" onClick={closeModal}>Close</button>
           </div>
         </div>
       </dialog>
