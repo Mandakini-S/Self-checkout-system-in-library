b665d77bc87228c9eedb6bfc6b268c1b1bafc458
mandakini <mandakinisapkota@gmail.com>
2024-03-04 08:20:22 +0545

    Add functionality to AddModal.jsx

--- frontend/src/Components/Modal/AddModal.jsx
+++ frontend/src/Components/Modal/AddModal.jsx
@@ -9,8 +9,46 @@ const AddModal = ({ showModal, closeModal }) => {
   const handleContentClick = (event) => {
     event.stopPropagation();
     // Simulate scanning a book and navigate to another page
-    navigate('/addbook');
+    // navigate('/addbook');
   };
+
+  const handleRFIDScan = async () => {
+    const port = await navigator.serial.requestPort();
+    await port.open({ baudRate: 115200 });
+
+    const textDecoder = new TextDecoderStream();
+    const readableStreamClosed = port.readable.pipeTo(textDecoder.writable);
+    const textReader = textDecoder.readable.getReader();
+    
+
+    // while (port.readable && keepReading){
+    //     reader = port.readable.getReader();
+    // }
+    try {
+        while (true) {
+            const { value, done } = await textReader.read();
+            if (done) {
+                textReader.releaseLock();
+                break;
+            }
+
+            console.log(value);
+
+            // Send the scanned value to the backend
+            navigate("/addbook", { state: { value } })
+            // sendToBackend(value);
+            //await readable.close();
+            await readableStreamClosed;
+        }
+    } catch (error) {
+        console.error('Error reading RFID data', error);
+    }
+    textReader.cancel();
+    await port.close();
+    await port.forget();
+
+
+};
 
   return (
     <div className={`modal-overlay ${showModal ? 'modal-open' : ''}`} onClick={closeModal}>
@@ -20,7 +58,9 @@ const AddModal = ({ showModal, closeModal }) => {
           <h3 id="text2">the tag in the book below for adding the book.</h3>
           <FontAwesomeIcon icon={faArrowDown} style={{ color: '#4D90FE', fontSize: '40px' }} />
           {/* Simulated button for scanning RFID card */}
-          
+          <button onClick={handleRFIDScan} id="openSerialPort" className="scan-button">
+                    Scan RFID Card
+                </button>
           {/* Add content specific to returning book modal */}
           <button className="btn" onClick={closeModal}>Close</button>
         </div>
